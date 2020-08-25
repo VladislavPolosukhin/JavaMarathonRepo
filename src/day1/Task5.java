1ecb91f7121b7ba5aed34e7da9b328fcc1712ab5
Vladislav <polosukhinvladislav@gmail.com>
2020-08-25 17:08:45 +0300

    correct task5 day1

--- src/day1/Task5.java
+++ src/day1/Task5.java
@@ -2,14 +2,14 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-        int age = 1;
+        int age;
         String sign = "Мой возраст ";
 
-        for (int i = 0; i < 20 ; i++) {
-            if (age < 100){
-                System.out.println(sign + age);
-            }
-            age = age + 5;
+        for (age = 7; age < 100; age = age + 5) {
+
+            System.out.println(sign + age);
+
+
         }
     }
 
