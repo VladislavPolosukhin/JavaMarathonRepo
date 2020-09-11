db5b311e7445a054f1c95c3362a7b402593dba9e
Vladislav <polosukhinvladislav@gmail.com>
2020-09-11 12:47:09 +0300

    day 18 corrections

--- src/day18/Task1.java
+++ src/day18/Task1.java
@@ -1,16 +1,17 @@
 package day18;
 
 public class Task1 {
-    static int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+
 
     public static void main(String[] args) {
-        System.out.println(summOfArray(numbers.length - 1));
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        System.out.println(summOfArray( numbers,numbers.length - 1));
     }
 
-    public static int summOfArray(int position) {
+    public static int summOfArray(int [] numbers ,int position) {
         if (position == -1)
             return 0;
 
-        return numbers[position] + summOfArray(--position);
+        return numbers[position] + summOfArray(numbers,--position);
     }
 }
--- src/day18/Task2.java
+++ src/day18/Task2.java
@@ -4,23 +4,22 @@ public class Task2 {
     private static int counter = 0;
 
     public static void main(String[] args) {
-        System.out.println(countSeven(61777123));
+        System.out.println(countSeven(71777127));
     }
 
     private static int countSeven(int number) {
-        if (number < 10) {
-            if (number == 7) {
-                return counter++;
-            }
-            return counter;
 
+        if (number == 0)
+        {
+            return counter;
         }
+
+
         if (number % 10 == 7) {
             counter++;
         }
-        String s = String.valueOf(number);
-        s = s.substring(0, s.length() - 1);
-        countSeven(Integer.parseInt(s));
+
+        countSeven(number / 10);
         return counter;
     }
 }
